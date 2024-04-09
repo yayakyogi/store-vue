00ff86575a923c3cf2c8c6057be8264fbf6c15d5
Yayak Yogi <yayaktaka@gmail.com>
2024-04-09 16:28:40 +0700

    thank you page

---
+++ src/views/TransactionSuccessView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrapper">
+    <img src="/images/illustrations/transaction-success.svg" />
+    <h1 class="title">Transaction Processed!</h1>
+    <p class="description">Silahkan tunggu konfirmasi email dari kami dan kami akan menginformasikan resi secept mungkin!</p>
+    <a-button size="large" type="primary">My Dashboard</a-button>
+    <a-button size="large">Go Shopping</a-button>
+  </div>
+</template>
+
+<style lang="less">
+.wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+
+  .title {
+    margin-bottom: 0;
+    line-height: 15px;
+  }
+
+  .description {
+    margin-bottom: 10px;
+    color: @grey-color;
+    max-width: 500px;
+    text-align: center
+  }
+}
+</style>
